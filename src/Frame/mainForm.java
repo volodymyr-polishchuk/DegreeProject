489693c993d18cec81d2c75a27e611adb983f7ed
Volodymyr Polishchuk <thevivalley@gmail.com>
2018-01-09 23:09:30 +0200

    Створення головної форми

--- src/Frame/mainForm.java
+++ src/Frame/mainForm.java
@@ -1,37 +1,90 @@
 package Frame;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.event.TableModelListener;
-import javax.swing.table.TableModel;
-import java.awt.*;
+import java.io.IOException;
 
 /**
  * Created by Vladimir on 09/01/18.
  **/
 public class mainForm extends JFrame {
-    private JPanel panel1;
-    private JTable table1;
+    private JPanel mainPanel;
+    private JTabbedPane tabbedPane;
+    private JLabel statusBar;
+    private JToolBar toolBar;
 
     public mainForm() {
-        setContentPane(panel1);
+        setJMenuBar(new myMenuBar());
+
+        setContentPane(mainPanel);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(new Dimension(400, 400));
+        setSize(800, 600);
+        setExtendedState(MAXIMIZED_BOTH);
+        setTitle("Система автоматизації складання розкладу занять");
+        InitJToolBar(toolBar);
         setVisible(true);
-        String[][] data = {{"Some text", "SomeStr"}, {"Somete", "Other"}};
-        String[] col = {"Srint", "Strong"};
-        table1.setModel(new JTable(data, col).getModel());
-        System.out.println(table1.getValueAt(0, 0).toString());
     }
 
-    public static void main(String[] args) {
-        mainForm mf = new mainForm();
+    public void addTab(JPanel jPanel) {
+        tabbedPane.add(jPanel);
+    }
+
+    public void setStatusBar(String s) {
+        statusBar.setText(s);
+    }
+
+    private void InitJToolBar(JToolBar jToolBar) {
+        try {
+            jToolBar.add(new JToggleButton(new ImageIcon(ImageIO.read(getClass().getResourceAsStream("/resource/cloud-computing.png")))));
+//            jToolBar.add(new JToggleButton(new ImageIcon(ImageIO.read(getClass().getResourceAsStream("/resource/cloud-computing.png")))));
+            //Додання панелі інструментів
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
-class MyTableObj {
-    @Override
+class myMenuBar extends JMenuBar {
+    myMenuBar() {
+// Створення головного меню програми
+        JMenu m1 = new JMenu("Програма");
+        m1.add(new JMenuItem("Виконати підключення до іншого сервера"));
+        m1.add(new JMenuItem("Налаштування"));
+        m1.add(new JPopupMenu.Separator());
+        m1.add(new JMenuItem("Вихід"));
+        add(m1);
+        JMenu m2 = new JMenu("Навчальний графік");
+        m2.add(new JMenuItem("Створити навчальний графік"));
+        m2.add(new JMenuItem("Редагувати існуючий графік"));
+        m2.add(new JMenuItem("Видалити або архівувати графік"));
+        add(m2);
+        JMenu m3 = new JMenu("Розклад занять");
+        m3.add(new JMenuItem("Створити розклад занять"));
+        m3.add(new JMenuItem("Редагувати існуючих розклад"));
+        m3.add(new JMenuItem("Видалити або архівувати розклад"));
+        add(m3);
+        JMenu m4 = new JMenu("Дані");
+        m4.add(new JMenuItem("Аудиторії"));
+        m4.add(new JMenuItem("Викладачі"));
+        m4.add(new JMenuItem("Предмети"));
+        m4.add(new JMenuItem("Навчальний предмет"));
+        add(m4);
 
-    public String toString() {
-        return super.toString();
+    }
+}
+
+class Test000 {
+    public static void main(String[] args) {
+        mainForm form = new mainForm();
+        form.setStatusBar("Всі дані завантажені до програми");
+        JPanel panel = new JPanel();
+        String[][] data = {{"Завірюха В. П.", "402", "Основи програмування"},
+                {"Харченко О. О.", "403", "ІСТО"},
+                {"Заболотній Ю. Л.", "407", "WEB"}};
+        String[] col = {"Викладач", "Аудиторія", "Предмет"};
+        JTable table = new JTable(data, col);
+        panel.add(table);
+        panel.setName("Викладачі");
+        form.addTab(panel);
     }
 }
