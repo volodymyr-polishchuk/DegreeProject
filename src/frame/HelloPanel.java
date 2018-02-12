57862a874712537534c8b1758661a1182c0e355f
Volodymyr Polishchuk <thevivalley@gmail.com>
2018-02-12 23:47:15 +0200

    Редагування вітального вікна

--- src/frame/HelloPanel.java
+++ src/frame/HelloPanel.java
@@ -1,15 +1,21 @@
 package frame;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Vladimir on 23/01/18.
  **/
 public class HelloPanel extends JPanel {
     private JPanel contentPane;
+    private JList list1;
+    private JButton перейтиButton;
+    private JButton перейтиButton1;
+    private JButton перейтиButton2;
 
     public HelloPanel(String name) {
         setName(name);
+        setLayout(new GridLayout());
         add(contentPane);
     }
 }
